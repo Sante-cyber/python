0a97653739b6c5b135e8413a5b8315a529ac5a4d
Sante-cyber <lyjjack.li@gmail.com>
2024-04-10 23:03:52 +1000

    update

--- bolling_band_back_trade_NZDCAD_real_rsi.py
+++ bolling_band_back_trade_NZDCAD_real_rsi.py
@@ -52,7 +52,7 @@ def market_order(symbol,volume,order_type,deviation,magic,stoploss,takeprofit):
 def get_signal(symbol):
     bars=mt.copy_rates_from_pos(symbol,TIMEFRAME,1,SMA_PERIOD)
     df=pd.DataFrame(bars)
-    overbought=68
+    overbought=70
     oversold=28
     df['time']=pd.to_datetime(df['time'],unit='s')
     df['hour']=df['time'].dt.hour
